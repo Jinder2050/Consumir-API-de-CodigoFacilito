c3d0172aa148074ebc983015ec771aa36a7787fb
Jinder2050 <yindercondori@gmail.com>
2023-03-11 14:38:47 -0500

    Add logging

--- pokeapi/__main__.py
+++ pokeapi/__main__.py
@@ -1,11 +1,21 @@
 from pokeapi import pokeapi
+import logging
 
 
+"""
+INFO -> 10
+debug -> 20
+WARNING -> 30
+ERROR -> 4
+CRITICAL ->4
+"""
+
+logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
 
-    print('>>> Ejecutando')
+    logging.debug('>>> Ejecutando')
     pokemons = pokeapi()
-    print(pokemons)
+    logging.debug(pokemons)
 
-    print('Finalizando')
+    logging.debug('Finalizando')    
